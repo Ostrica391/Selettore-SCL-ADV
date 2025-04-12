ff2785a7a923ad682e1ae309a1718e2c77cd8cba
Ostrica391 <alti94@hotmail.it>
2025-04-12 15:41:02 +0200

    Update selettore_lenti.py

--- selettore_lenti.py
+++ selettore_lenti.py
@@ -46,8 +46,8 @@ st.markdown("""
     <div class="top-menu">
         <a href="https://selettore-cs.streamlit.app/" target="_blank">Calcolatore CS</a>
         <a href="https://selettore-scl-adv.streamlit.app/" target="_blank">Calcolatore SCL-ADV</a>
-        <a href="https://link3.com" target="_blank">Work in progress</a>
-        <a href="https://link4.com" target="_blank">Work in progress</a>
+        <a href="https://www.tslac.it/">Work in progress</a>
+        <a href="https://www.tslac.it/">Work in progress</a>
     </div>
 """, unsafe_allow_html=True)
 
